f11ff157d95a47a61aa723d57c3508b51201fc38
Michael Snoyman <michael@snoyman.com>
2016-02-08 08:14:50 +0200

    Skip tests when no package index available (fixes fpco/stackage#1165)

--- test/Stackage/PackageIndexSpec.hs
+++ test/Stackage/PackageIndexSpec.hs
@@ -5,11 +5,12 @@ import Stackage.PackageIndex
 import Stackage.Prelude
 import Test.Hspec
 import Distribution.Package (packageId)
+import System.Directory (doesFileExist, getAppUserDataDirectory)
 
 spec :: Spec
 spec = do
-    it "works" $ (runResourceT $ sourcePackageIndex $$ sinkNull :: IO ())
-    it "getLatestDescriptions gives reasonable results" $ do
+    it "works" $ ifIndexExists $ (runResourceT $ sourcePackageIndex $$ sinkNull :: IO ())
+    it "getLatestDescriptions gives reasonable results" $ ifIndexExists $ do
         let f x y = (display x, display y) `member` asSet (setFromList
                 [ (asText "base", asText "4.5.0.0")
                 , ("does-not-exist", "9999999999999999999")
@@ -19,3 +20,12 @@ spec = do
         p <- simpleParse $ asText "base"
         v <- simpleParse $ asText "4.5.0.0"
         (spdVersion <$> m) `shouldBe` singletonMap p v
+
+ifIndexExists :: IO () -> IO ()
+ifIndexExists inner = do
+    stack <- getAppUserDataDirectory "stack"
+    let fp = stack </> "indices" </> "Hackage" </> "00-index.tar"
+    exists <- doesFileExist fp
+    if exists
+        then inner
+        else pendingWith "00-index.tar not available, skipping test"
